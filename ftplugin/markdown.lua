d3fa1053abf4fee529cb02f8ee55db99b41d0709
qwavies <qwavsbusiness@gmail.com>
2026-01-16 23:46:35 +1100

    feat: Added markdown specific logic for markdown files

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -4,3 +4,5 @@ vim.opt_local.shiftwidth = tab_size
 vim.opt_local.tabstop = tab_size
 
 vim.opt.wrap = false
+
+vim.keymap.set("n", "<leader>h", "<cmd>Markview toggle<CR>", { buffer = true, desc = "Toggles markview"})
