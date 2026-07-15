c5ef19d1d948f37b2e57afbfec65f9aec16cdd2d
qwavies <qwavsbusiness@gmail.com>
2026-07-15 21:57:34 +1000

    feat(markview): added keymaps in markdown and typst files

--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -1,8 +1,3 @@
-local tab_size = 2
-
-vim.opt_local.shiftwidth = tab_size
-vim.opt_local.tabstop = tab_size
-
 vim.opt_local.wrap = false
 
 vim.keymap.set("n", "<leader>h", "<cmd>Markview toggle<CR>", { buffer = true, desc = "Toggles markview"})
